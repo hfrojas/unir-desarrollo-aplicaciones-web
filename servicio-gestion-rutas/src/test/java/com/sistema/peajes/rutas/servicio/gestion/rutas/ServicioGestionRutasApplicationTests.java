6f027e5b8cf60b8c63e8c50adfb427324c39dea0
Harold Fernando Rojas Lopez <hrojas@hiberus.com>
2024-11-27 20:49:49 -0500

    Proyecto inicial para el microservicio gestion-rutas

---
+++ servicio-gestion-rutas/src/test/java/com/sistema/peajes/rutas/servicio/gestion/rutas/ServicioGestionRutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.peajes.rutas.servicio.gestion.rutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioGestionRutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
