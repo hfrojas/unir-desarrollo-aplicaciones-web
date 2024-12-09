f3a7b1aadb12cfe973d86722a82a834b8fff2dec
Harold Fernando Rojas Lopez <hrojas@hiberus.com>
2024-12-08 23:17:17 -0500

    Se configura el Apigateway

---
+++ servicio-gateway/src/main/java/com/sistema/peajes/servicio_gateway/ServicioGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.peajes.servicio_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioGatewayApplication.class, args);
+	}
+
+}
---
+++ servicio-gateway/src/test/java/com/sistema/peajes/servicio_gateway/ServicioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.peajes.servicio_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
