a300ae47807b24ab44c6eaa3281ac6c03281be04
harold rojas <haroldrojaslopez@gmail.com>
2025-01-12 04:17:09 -0500

    Se ajusta IP de eureka a localhost, se agrega configuraciòn de CORS al apigateway

---
+++ servicio-gateway/src/main/java/com/sistema/peajes/servicio_gateway/config/GatewayCorsConfig.java
@@ -0,0 +1,26 @@
+package com.sistema.peajes.servicio_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+@Configuration
+public class GatewayCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:5173");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
